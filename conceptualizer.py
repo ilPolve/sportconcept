191461011b77d2f696f5519e7d696532edf74edc
Giuseppe Carrino <josephcarrinospam@gmail.com>
2021-10-11 22:11:06 +0200

    Trying translations and first pos_tag

--- conceptualizer.py
+++ conceptualizer.py
@@ -4,7 +4,10 @@ import os
 from googletrans import Translator
 import copy
 import time
-
+import httpx
+#jshint ignore:start
+import spacy
+#jshint ignore:end
 
 nations= ['CH', 'DE', 'FR', 'IT', 'UK', 'US']
 edition_str_len= 15
@@ -15,6 +18,19 @@ nation_to_lang= {'CH': 'fr',
                  'IT': 'it',
                  'UK': 'en',
                  'US': 'en'}
+
+
+fr_nlp= spacy.load("fr_core_news_sm")
+de_nlp= spacy.load("fr_core_news_sm")
+it_nlp= spacy.load("it_core_news_sm")
+en_nlp= spacy.load("en_core_web_sm")
+
+nlp_dict= {'CH': fr_nlp,
+           'FR': fr_nlp,
+           'DE': de_nlp,
+           'UK': en_nlp,
+           'US': en_nlp,
+           'IT': it_nlp}
 
 editions= []
 flow= []
@@ -36,7 +52,8 @@ def unicode_fix(to_fix):
 def to_english(new, lang):
     curr_news_en= copy.deepcopy(new)
     service_urls=['translate.google.com']
-    translator = Translator(service_urls= service_urls)
+    timeout = httpx.Timeout(5)
+    translator = Translator(service_urls= service_urls, timeout= timeout)
     translator.raise_Exception = True
     curr_news_en['title']= translator.translate(new['title'], src= lang, dest= "en").text
     print(curr_news_en)
@@ -48,11 +65,11 @@ def to_english(new, lang):
 
 #Function which populates arrays of news, based on eidition or flow type of news
 def news_finder(nation):
-    directory= "..\\D_Coll\\collectedNews\\" + nation
+    directory= "../newScraping/collectedNews/" + nation
     for subdir in os.scandir(directory):
         newspaper= subdir.name
         for news in os.scandir(subdir):
-            f= open(directory + "\\" + newspaper + "\\" + news.name, "r+")
+            f= open(directory + "/" + newspaper + "/" + news.name, "r+", encoding= "latin-1")
             curr_news= json.load(f)
             #adding new conceptual informations to the news
             for curr_new in curr_news:
@@ -60,20 +77,42 @@ def news_finder(nation):
                 curr_new['source']= newspaper
                 curr_new['filename']= news.name
             lang= nation_to_lang[nation]
-            if lang != 'en':
+            #time.sleep(0.1)
+            ##if lang != 'en':
                 #now create a hardcopy in order to have the same new, but in English
-                curr_news_en= to_english(curr_new, lang)
+                #curr_news_en= to_english(curr_new, lang)
             if len(news.name) == edition_str_len:
                 editions.append(curr_news)
-                editions.append(curr_news_en)
+                #editions_en.append(curr_news_en)
             else:
                 flow.append(curr_news)
-                flow.append(curr_news_en)
+                #flow_en.append(curr_news_en)
  
 
-print("aaaaassss")
 for nation in nations:
     news_finder(nation)
 flow= unicode_fix(flow)
-print(editions_en)
-print(flow_en)
+#print(editions_en)
+#print(flow_en)
+
+for edition in editions:
+    nlp= de_nlp
+    for new in edition:
+        #if new['nation']== "CH" or new['nation']=="FR":
+            #nlp= fr_nlp
+        #elif new['nation']== "UK" or new['nation']=="US":
+            #nlp= en_nlp
+        #elif new['nation']== "IT":
+            #nlp= it_nlp
+        nlp= nlp_dict[new['nation']]
+        doc_title= nlp(new['title'])
+        for t_token in doc_title:
+            print(t_token.text, t_token.dep_, t_token.head.pos_)
+        try:
+            doc_content= nlp(new['content'])
+            for c_token in doc_content:
+                print(c_token.text, c_token.dep_, c_token.head.pos_)
+        except:
+            pass
+        
+        
