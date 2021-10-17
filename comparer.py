65ed91c5f0ad56cf63c97c33c82ab685e29318e6
Giuseppe Carrino <josephcarrinospam@gmail.com>
2021-10-17 15:02:02 +0200

    Array di array per migliore gestione dei compare

--- comparer.py
+++ comparer.py
@@ -6,12 +6,14 @@ nations= ['CH', 'DE', 'FR', 'IT', 'UK', 'US']
 ed_type= ["RTS", "Tagesschau", "Zdf", "France24", "GR1", "PBS"]
 flow_type= ["Zeit", "Televideo", "ilPost", "BBC", "NYT"]
 
-editions_by_source= {'RTS': [], 'Tagesschau': [], 'Zdf': [], 'France24': [], 'GR1': [], 'PBS': []}
-flows_by_source= {'Zeit': [], 'Televideo': [], 'ilPost': [], 'BBC': [], 'NYT': []}
+source_conv= {'RTS': 0, 'Tagesschau': 1, 'Zdf': 2, 'France24': 3, 'GR1': 4, 'PBS': 5,
+              'Zeit': 0, 'Televideo': 1, 'ilPost': 2, 'BBC': 3, 'NYT': 4}
+
+editions_by_source= [[], [], [], [], [], []]
+flows_by_source= [[], [],[], [], []]
 
 def jsonizer(directory):
     f= open(directory, "r+")
-    print(directory)
     to_ret= json.load(f)
     f.close()
     return to_ret
@@ -21,10 +23,10 @@ def news_getter(nation, source):
     print(source)
     if source in ed_type:
         for to_append in os.scandir(directory):
-            editions_by_source[source].append(jsonizer(directory + "/" + to_append.name))
+            editions_by_source[source_conv[source]].append(jsonizer(directory + "/" + to_append.name))
     elif source in flow_type:
             for to_append in os.scandir(directory):
-                flows_by_source[source].append(jsonizer(directory + "/" + to_append.name))
+                flows_by_source[source_conv[source]].append(jsonizer(directory + "/" + to_append.name))
     else:
         print("Source not recognized")
             
