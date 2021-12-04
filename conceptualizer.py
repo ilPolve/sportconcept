4e6558a3c47ce0c8f3986b0ca8440e4e36bcccc8
Giuseppe Carrino <josephcarrinospam@gmail.com>
2021-12-04 19:22:20 +0100

    conceptualizer modificato

--- conceptualizer.py
+++ conceptualizer.py
@@ -22,7 +22,18 @@ def pos_tagger(editions, my_subdir):
         for edition in nat_ed:
             c_edit= []
             for new in edition:
-                concepts= []
+                conceptitle = conceptualize(new)
+                c_edit.append(conceptitle)
+            to_ret.append(c_edit)
+            if len(edition) > 0:
+                os.remove(basedir + my_subdir + "/" + edition[0]['source'] + "/" + edition[0]['filename'][5:])
+                f= open(basedir + my_subdir + "/" + edition[0]['source'] + "/conc_" + edition[0]['filename'][5:], "w") 
+                json.dump(edition, f, ensure_ascii = False, indent= 4)
+                f.close()
+    return to_ret
+
+def conceptualize(new):
+    concepts= []
                 #if new['my_subdir']== "CH" or new['my_subdir']=="FR":
                     #nlp= fr_nlp
                 #elif new['my_subdir']== "UK" or new['my_subdir']=="US":
@@ -31,11 +42,11 @@ def pos_tagger(editions, my_subdir):
                     #nlp= it_nlp
                 #Choosing the right nlp-parser due to the news languages
                 #nlp-ing the title
-                doc_title= nlp(new['en_title'])
-                for t_token in doc_title:
+    doc_title= nlp(new['en_title'])
+    for t_token in doc_title:
                     #print(t_token.text, t_token.dep_, t_token.head.pos_)
-                    if t_token.pos_ == "NOUN" or t_token.pos_ == "PROPN":
-                        concepts.append(create_concept(t_token.text, t_token.dep_, t_token.head.pos_))
+        if t_token.pos_ == "NOUN" or t_token.pos_ == "PROPN":
+            concepts.append(create_concept(t_token.text, t_token.dep_, t_token.head.pos_))
                 #this s a try only because some news may not have a content field, in that case just skip
                 #currently not using it because it tends to analyze "sentence-per-sentence", going out of the project target
                 #try:
@@ -46,26 +57,19 @@ def pos_tagger(editions, my_subdir):
                             #concepts.append(create_concept(c_token.text, c_token.dep_, c_token.head.pos_))
                 #except:
                     #pass
-                try:
-                    act_date = new['date']
-                except:
-                    act_date = new['date_raw']
-                conceptitle={'title': new['title'],
+    try:
+        act_date = new['date']
+    except:
+        act_date = new['date_raw']
+    conceptitle={'title': new['title'],
                             'en_title': new['en_title'],
                             'date': act_date,
                             'language': new['language'],
                             'source': new['source'],
                             'concepts': concepts}
-                new['concepts']= concepts
-                #print(json.dumps(conceptitle, indent= 4))
-                c_edit.append(conceptitle)
-            to_ret.append(c_edit)
-            if len(edition) > 0:
-                os.remove(basedir + my_subdir + "/" + edition[0]['source'] + "/" + edition[0]['filename'][5:])
-                f= open(basedir + my_subdir + "/" + edition[0]['source'] + "/conc_" + edition[0]['filename'][5:], "w") 
-                json.dump(edition, f, ensure_ascii = False, indent= 4)
-                f.close()
-    return to_ret
+    new['concepts']= concepts
+    #print(json.dumps(conceptitle, indent= 4))
+    return conceptitle
         
 
 def getting_news(my_subdir):
