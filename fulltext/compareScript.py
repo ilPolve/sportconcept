cb55c83d9e088833bb77989be3fedeebc3f7cf46
JosephCarrino <josephcarrinospam@gmail.com>
2022-04-28 18:25:15 +0200

    compareScript: added new function for the whole day comparison

--- fulltext/compareScript.py
+++ fulltext/compareScript.py
@@ -12,14 +12,15 @@ DATES = ["2022-04-22", "2022-04-21"]
 
 START_HOUR = 18
 
-DONE = [f"{BASE_URL}EN/CNN - {BASE_URL}FR/France24", f"{BASE_URL}EN/CNN - {BASE_URL}DE/Spiegel", f"{BASE_URL}EN/CNN - {BASE_URL}IT/ilPost"]
+DONE = []
 
 def main():
     for i in range(0, len(TO_DO)):
         for j in range(i+1, len(TO_DO)):
             if f"{BASE_URL}{TO_DO[i]} - {BASE_URL}{TO_DO[j]}" not in DONE:
                 print(TO_DO[i] + " " + TO_DO[j])
-                np_comparer(TO_DO[i], TO_DO[j])
+                #np_comparer(TO_DO[i], TO_DO[j])
+                #fullday_comparer(TO_DO[i], TO_DO[j])
 
 def np_comparer(newsp_A, newsp_B):
     curr_date = DATES[0]
@@ -28,6 +29,10 @@ def np_comparer(newsp_A, newsp_B):
         full_comparer(f"{BASE_URL}{newsp_A}", f"{BASE_URL}{newsp_B}", curr_date, str(curr_hour), cosine=True)
         if curr_hour == START_HOUR-1:
             curr_date = DATES[1]
+
+def fullday_comparer(newsp_A, newsp_B):
+    full_comparer(f"{BASE_URL}{newsp_A}", f"{BASE_URL}{newsp_B}", DATES[1], str(START_HOUR))
+    full_comparer(f"{BASE_URL}{newsp_A}", f"{BASE_URL}{newsp_B}", DATES[1], str(START_HOUR), cosine=True)
 
 if __name__ == "__main__":
     main()
