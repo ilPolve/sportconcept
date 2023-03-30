15e251c6a20dadb406f66f1dbeb700cefd981dcd
JosephCarrino <josephcarrinospam@gmail.com>
2023-03-30 23:54:41 +0200

    visualization: fixed annotation with number exclusive news and x/ylabel for changing rates

--- HT_2023/visualization/sce.py
+++ HT_2023/visualization/sce.py
@@ -18,9 +18,9 @@ def sce_visual():
                 sce_data[int(idx)] = json.load(fp)
 
     sorted_data = sorted(sce_data)
-    skipped = {"DE\\Spiegel": [], "EN\\BBC": [], "ES\\ABC": [], "FR\\France24": [], "IT\\ilPost": []}
-    common =  {"DE\\Spiegel": [], "EN\\BBC": [], "ES\\ABC": [], "FR\\France24": [], "IT\\ilPost": []}
-    exclusive =  {"DE\\Spiegel": [], "EN\\BBC": [], "ES\\ABC": [], "FR\\France24": [], "IT\\ilPost": []}
+    skipped = {"FR\\France24": []}
+    common =  {"FR\\France24": []}
+    exclusive =  {"FR\\France24": []}
     for key in skipped:
         for idx in sorted_data:
             total_news = sce_data[idx][key]["skipped"] + sce_data[idx][key]["common"] + sce_data[idx][key]["exclusive"]
@@ -29,24 +29,33 @@ def sce_visual():
             exclusive[key].append(round(sce_data[idx][key]["exclusive"] / total_news, DIGITS))
 
     for key in skipped:
-        plt.plot(sorted_data, skipped[key], label="Skipped")
-        plt.plot(sorted_data, common[key], label="Common")
-        plt.plot(sorted_data, exclusive[key], label="Exclusive")
+        plt.plot(sorted_data, skipped[key], "-o", label="Skipped")
+        plt.plot(sorted_data, common[key], "-o", label="Common")
+        plt.plot(sorted_data, exclusive[key], "-o", label="Exclusive")
         plt.title(f"Rate of S.C.E. news of {key}")
+        plt.xlabel("Considered hours")
+        plt.ylabel("News rate")
         plt.legend()
         plt.show()
 
-        plt.plot(sorted_data, skipped[key], label="Skipped")
+        plt.plot(sorted_data, skipped[key], "-o", label="Skipped")
         plt.title(f"Skipped news of {key}")
+        plt.xlabel("Considered hours")
+        plt.ylabel("News rate")
         plt.show()
 
-        plt.plot(sorted_data, common[key], label="Common")
+        plt.plot(sorted_data, common[key], "-o", label="Common")
         plt.title(f"Common news of {key}")
+        plt.xlabel("Considered hours")
+        plt.ylabel("News rate")
         plt.show()
 
-        plt.plot(sorted_data, exclusive[key], label="Exclusive")
+        plt.plot(sorted_data, exclusive[key], "-o", label="Exclusive")
         plt.title(f"Exclusive news of {key}")
+        plt.xlabel("Considered hours")
+        plt.ylabel("News rate")
         plt.show()
+        #sce of france24
     
 if __name__ == "__main__":
     main()
--- HT_2023/visualization/triple_def_graph.py
+++ HT_2023/visualization/triple_def_graph.py
@@ -2,13 +2,15 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
+hours = 24
+
 def main():
     triple_def_visual()
 
 def triple_def_visual():
     triple_def_data = {}
     color = [1, 25, 50, 100, 150, 200, 300, 450, 550, 600, 650]
-    with open("triple_def_out_12_ANSA.json") as fp:
+    with open(f"triple_def_out_{hours}.json") as fp:
         triple_def_data = json.load(fp)
     names = []
     x_skip = []
@@ -20,12 +22,14 @@ def triple_def_visual():
         x_skip.append(triple_def_data[source]["skipped"])
         y_com.append(triple_def_data[source]["common"])
         z_excl.append(triple_def_data[source]["exclusive"])
+    title = "S.C.E. news considering "
+    hours_title = "1 snapshot" if hours == 1 else f"{hours} hours"
     plt.scatter(x_skip, y_com, s=[100*(z+1) for z in z_excl], c = color[:len(x_skip)])
     for i, txt in enumerate(names):
-        plt.annotate(txt, ((z_excl[i])/100 + (x_skip[i]+1), (z_excl[i]+1)/100 + (y_com[i])))
+        plt.annotate(f"{txt}  [{z_excl[i]}]", ((z_excl[i])/100 + (x_skip[i]+1), (z_excl[i]+1)/100 + (y_com[i])))
     plt.xlabel("Skipped news")
     plt.ylabel("Covered news")
-    plt.title("Scatter size -> Exclusive news")
+    plt.title(f"{title}{hours_title}")
     plt.show()
     return
 
