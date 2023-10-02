4d49ced2611d509a801cd98510d617565c3d5596
JosephCarrino <josephcarrinospam@gmail.com>
2023-10-02 23:11:12 +0200

    Done SCE on 5 days (03-11 to 03-15)

--- HT_2023/triple_def.py
+++ HT_2023/triple_def.py
@@ -6,7 +6,7 @@ import json
 import os
 import random
 from typing import Optional, Union, List, Tuple
-from utils import snapped_news_by_source, has_similar_in_snapshot, snapped_news_in_range, Snapshot
+from utils import snapped_news_by_source, has_similar_in_snapshot, snapped_news_in_range, Snapshot, similar_in_snapshot_linked, similar_in_snapshot_spacy
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -21,9 +21,23 @@ check_dir = "ANSA_Politica"
 to_check_dir = f"{repo_dir}/{snap_dir}/{news_snap}"
 
 # For real example:
-repo_dir = f"{BASE_DIR}\\..\\fulltext\\NER\\flow"
-ALL_NEWS_OUTLETS = ["IT\\ilPost", "ES\\ABC", "FR\\France24", "EN\\BBC", "DE\\Spiegel"]
+repo_dir = f"{BASE_DIR}\\..\\fulltext\\translated"
+ALL_NEWS_OUTLETS = ["IT\\ilPost", "ES\\ABC", "FR\\France24", "EN\\BBC", "DE\\Spiegel", "IT\\ANSA_Esteri"]
 
+
+# Trying Swissinfo
+# repo_dir = f"{BASE_DIR}\\..\\..\\SwissScrape\\scraped_items"
+# snap_dir = "ITA"
+# news_snap = "2023-09-23T18.16.42E1695493002.188904.json"
+# check_dir = "ITA"
+# to_check_dir = f"{repo_dir}\\{snap_dir}\\{news_snap}"
+# ALL_NEWS_OUTLETS = ["GER", "FRE", "ENG", "SPA"]
+
+NLPY = True
+
+SIMIL_FUN = similar_in_snapshot_linked
+if NLPY:
+    SIMIL_FUN = similar_in_snapshot_spacy
 
 
 def main():
@@ -33,17 +47,17 @@ def main():
 def skip_common_excl(to_check: dict, check_dir: str, simil_cache: dict = {}, in_range: bool = False, start_date: str = None, end_date: str = None) -> Union[str, List[str]]:
     main_snapped = []
     if in_range:
-        main_snapped, _ = snapped_news_in_range(f"{repo_dir}/{check_dir}", start_date, end_date)
+        main_snapped, _ = snapped_news_in_range(f"{repo_dir}/{check_dir}", start_date, end_date, nlpy=NLPY)
     else:
-        main_snapped = snapped_news_by_source(f"{repo_dir}/{check_dir}")
-    if not has_similar_in_snapshot(to_check, main_snapped):
+        main_snapped = snapped_news_by_source(f"{repo_dir}/{check_dir}", nlpy = NLPY)
+    if not has_similar_in_snapshot(to_check, main_snapped, simil_fun=SIMIL_FUN):
         print("skipped")
         print([])
         return "skipped", []
     covering_list = []
     for dir in ALL_NEWS_OUTLETS:
-        snapped = snapped_news_by_source(f"{repo_dir}/{dir}")
-        if has_similar_in_snapshot(to_check, snapped, simil_cache):
+        snapped = snapped_news_by_source(f"{repo_dir}/{dir}", nlpy = NLPY)
+        if has_similar_in_snapshot(to_check, snapped, simil_cache, simil_fun=SIMIL_FUN):
             covering_list.append(dir)
 
     covering_list.append(check_dir)
@@ -61,13 +75,14 @@ def snapshots_in_range(
     in_range: bool,
     start_date: int,
     end_date: int,
+    nlpy: bool = NLPY,
 ) -> dict[str, Snapshot]:
     snapshots = {}
     for news_outlet in ALL_NEWS_OUTLETS:
         if in_range:
-            snapshot, _ = snapped_news_in_range(f"{repo_dir}/{news_outlet}", start_date, end_date)
+            snapshot, _ = snapped_news_in_range(f"{repo_dir}/{news_outlet}", start_date, end_date, nlpy=nlpy)
         else:
-            snapshot = snapped_news_by_source(f"{repo_dir}/{news_outlet}")
+            snapshot = snapped_news_by_source(f"{repo_dir}/{news_outlet}", nlpy = nlpy)
 
         snapshots[news_outlet] = snapshot
 
@@ -81,7 +96,7 @@ def sce_classify(
 ) -> tuple[list[str], list[str], list[str]]:
     sce_list = []
     for news_outlet, snapshot in snapshots.items():
-        if has_similar_in_snapshot(news_item_to_check, snapshot, simil_cache):
+        if has_similar_in_snapshot(news_item_to_check, snapshot, simil_cache, simil_fun=SIMIL_FUN):
             sce_list.append(news_outlet)
 
     skipped = []
@@ -103,9 +118,10 @@ def sce_classify(
 
 
 def main_news_getter(dir: str) -> dict:
-    with open(dir, "r") as f:
+    with open(dir, "r", encoding="utf-8") as f:
         news = json.load(f)
     n_new = random.randint(0, len(news)-1)
+    n_new = 0
     single_news = news[n_new]
     return single_news
 
--- HT_2023/visualization/triple_def_graph.py
+++ HT_2023/visualization/triple_def_graph.py
@@ -1,35 +1,73 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-hours = 24
+plt.rc('font', weight='bold')
+plt.rc('axes', axisbelow = True)
+
+FONTSIZE =24
+HOURS = [1]
+
+JSON_DIR = f"special_issue\\triple_def_out_prova.json"
 
 def main():
-    triple_def_visual()
+    for hour in HOURS:
+        triple_def_visual(hour)
 
-def triple_def_visual():
+def triple_def_visual(hours):
     triple_def_data = {}
-    color = [1, 25, 50, 100, 150, 200, 300, 450, 550, 600, 650]
-    with open(f"triple_def_out_{hours}.json") as fp:
+    with open(JSON_DIR) as fp:
         triple_def_data = json.load(fp)
     names = []
     x_skip = []
     y_com = []
     z_excl = []
     for source in triple_def_data:
+        if source != "IT\\ANSA_Esteri":
         # names.append(source.split("/")[1])
-        names.append(source.split("\\")[1])
-        x_skip.append(triple_def_data[source]["skipped"])
-        y_com.append(triple_def_data[source]["common"])
-        z_excl.append(triple_def_data[source]["exclusive"])
+            names.append(source.split("\\")[1])
+            x_skip.append(triple_def_data[source]["skipped"])
+            y_com.append(triple_def_data[source]["common"])
+            z_excl.append(triple_def_data[source]["exclusive"])
+    names.append("ANSA_Esteri")
+    x_skip.append(triple_def_data["IT\\ANSA_Esteri"]["skipped"])
+    y_com.append(triple_def_data["IT\\ANSA_Esteri"]["common"])
+    z_excl.append(triple_def_data["IT\\ANSA_Esteri"]["exclusive"])
+    tot_news = triple_def_data["DE\\Spiegel"]["skipped"] + triple_def_data["DE\\Spiegel"]["common"] + triple_def_data["DE\\Spiegel"]["exclusive"]
+    if "ANSA_Esteri" in names:
+        x_c = np.arange(len(x_skip)-1)
+        y_c = [i+x_c+(i*x_c)**2 for i in range(len(x_skip)-1)]
+        colors = cm.rainbow(np.linspace(0, 1, len(y_c)))
+    x_c = np.arange(len(x_skip)+1)
+    y_c = [i+x_c+(i*x_c)**2 for i in range(len(x_skip)+1)]
+    ansa_color = cm.rainbow(np.linspace(0,1, len(y_c)))
+
     title = "S.C.E. news considering "
     hours_title = "1 snapshot" if hours == 1 else f"{hours} hours"
-    plt.scatter(x_skip, y_com, s=[100*(z+1) for z in z_excl], c = color[:len(x_skip)])
-    for i, txt in enumerate(names):
-        plt.annotate(f"{txt}  [{z_excl[i]}]", ((z_excl[i])/100 + (x_skip[i]+1), (z_excl[i]+1)/100 + (y_com[i])))
-    plt.xlabel("Skipped news")
-    plt.ylabel("Covered news")
-    plt.title(f"{title}{hours_title}")
+
+    print(tot_news)
+
+    # plot the diagonal line along all the graph
+    plt.plot([tot_news, 0], [0, tot_news], color="black", linestyle="--", linewidth=2)
+
+    plt.grid()
+    for i in range(len(x_skip)-1):
+        print(colors[i])
+        print(names[i])
+        print(i)
+        plt.scatter(x_skip[i], y_com[i], s=[400*(z_excl[i]+7)], color = colors[i], label=f"{names[i]} [{z_excl[i]}]")
+    plt.scatter(x_skip[len(x_skip)-1], y_com[len(y_com) - 1], s=[400*(z_excl[len(z_excl)-1] + 7)], color = [0.8, 0.1, 0.7, 1], label=f"{names[len(names)-1]} [{z_excl[len(z_excl)-1]}]")
+    # for i, txt in enumerate(names):
+    #     plt.annotate(f"[{z_excl[i]}]", ((z_excl[i]+5)/35 + (x_skip[i]+3), (z_excl[i]+5)/35 + (y_com[i]+3)), fontsize = FONTSIZE)
+    plt.xticks(fontsize=FONTSIZE-3)
+    plt.yticks(fontsize=FONTSIZE-3)
+    plt.xlabel("Skipped news", fontsize=FONTSIZE, weight="bold")
+    plt.ylabel("Covered news", fontsize=FONTSIZE, weight="bold")
+    plt.title(f"{title}{hours_title}", fontsize=FONTSIZE, weight="bold")
+    lgnd = plt.legend(fontsize=FONTSIZE-4)
+    for i in range(len(x_skip)):
+        lgnd.legendHandles[i]._sizes = [80]
     plt.show()
     return
 
